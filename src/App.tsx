c488007f64e90fb981fa87ff2e62929469d60d7e
ahilanananthakrishnan <aanantha@nd.edu>
2026-03-27 18:59:09 -0400

    Initial Motion Rehab setup

---
+++ src/App.tsx
@@ -0,0 +1,10 @@
+function App() {
+  return (
+    <main style={{ padding: "2rem", fontFamily: "Arial, sans-serif" }}>
+      <h1>Motion Rehab</h1>
+      <p>MVP setup is working.</p>
+    </main>
+  );
+}
+
+export default App;
